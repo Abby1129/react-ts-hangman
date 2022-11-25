658e45d6cd486238f544a4d19c660c01014aa2ea
Abby F <abby_faigao@yahoo.com>
2022-11-25 13:22:33 -0800

    app strcture planning

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import words from "./wordList.json";
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
   return <h1>Hi</h1>;
 }
 
