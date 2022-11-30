4618087b18305e1ed2201215bb5f1fbec6c94def
Abby F <abby_faigao@yahoo.com>
2022-11-29 22:22:16 -0800

    create game keyboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import { useState } from "react";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 import words from "./wordList.json";
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   });
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
-  return <h1>Hi</h1>;
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}></div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+      <div style={{ alignSelf: "stretch" }}></div>
+    </div>
+  );
 }
 
 export default App;
